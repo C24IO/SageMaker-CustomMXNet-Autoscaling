f87d1e34206f5ce982c46d0e25a6dcdbfdbd086a
Chaitanya Hazarey <c@24.io>
2020-06-11 21:32:44 +0000

    Port labs over from internal repo

---
+++ Lab1/.ipynb_checkpoints/build_and_push-checkpoint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# The name of our algorithm
+algorithm_name=chazarey-mxnet-serving-160-gpu-py3
+account=$(aws sts get-caller-identity --query Account --output text)
+
+# Get the region defined in the current configuration (default to us-west-2 if none defined)
+region=$(aws configure get region)
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
+
+# If the repository doesn't exist in ECR, create it.
+aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
+fi
+
+# Get the login command from ECR and execute it directly
+$(aws ecr get-login --region ${region} --no-include-email)
+
+# Build the docker image locally with the image name and then push it to ECR
+
+docker build -t ${algorithm_name} -f Dockerfile.gpu .
+docker tag ${algorithm_name} ${fullname}
+docker push ${fullname}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# SageMaker-CustomMXNet-Autoscaling
-SageMaker-CustomMXNet-Autoscaling
+POC Script 
+
+## Lab 1
+
+* Build a custom MXNet container for ResNet-152 model
+* Use protobuf payload to invoke SageMaker Endpoint
+* Enable SageMaker endpoint AutoScaling
+
+## Lab 2 
+
+* Build a custom MXNet container for Deformable-ConvNets based model 
+* Use protobuf payload to invoke SageMaker Endpoint
+* Enable SageMaker endpoint AutoScaling
